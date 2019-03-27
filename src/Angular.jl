5274a26ba9d138b0141d6bc0c99ce6e98d75e069
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-03-27 17:36:56 +1300

    Add halfint macro

--- src/Angular.jl
+++ src/Angular.jl
@@ -7,6 +7,38 @@ using WignerSymbols: HalfInteger, clebschgordan
 Base.round(::Type{HalfInteger}, x) = HalfInteger(round(Int, 2x), 2)
 Base.:*(a::HalfInteger, b::HalfInteger) = (a.numerator * b.numerator) // 4 # converts to Rational
 
+"""
+    @halfint(x) -> HalfInteger
+
+Performs a compile-time conversion of a number literal into a `HalfInteger`. This allows
+for an easy construction of `HalfInteger` values, from e.g. integers and rationals.
+
+```jldoctest
+julia> @halfint(3//2)
+3/2
+
+julia> @halfint(3//2) |> typeof
+HalfInteger
+```
+
+Trying to use `@halfint` on a non-literal value will yield a compile-time error:
+
+```jldoctest
+julia> function foo(q)
+           @halfint(q)
+       end
+ERROR: LoadError: ArgumentError: @halfint macro needs a compile-time constant as an argument
+```
+"""
+macro halfint(x)
+    value = try
+        eval(x)
+    catch
+        throw(ArgumentError("@halfint macro needs a compile-time constant as an argument"))
+    end
+    convert(HalfInteger, value)
+end
+
 include("combinatorics.jl")
 include("linearalgebra.jl")
 include("vectorspaces.jl")
